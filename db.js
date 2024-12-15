ea3b3f04816567158a2384379b3a250edbe7766e
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-12-15 18:07:59 +0000

    auth middleware

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
+const { promisify } = require('util');
 
 var db = new sqlite3.Database('iku.db', (err) => {
     if (err) {
@@ -9,18 +10,18 @@ var db = new sqlite3.Database('iku.db', (err) => {
 });
 
 db.serialize(() => {
-    db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT)');
+    db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT)');
     db.run('CREATE TABLE IF NOT EXISTS tracks (id INTEGER PRIMARY KEY, title TEXT, artist TEXT, album TEXT, year INTEGER, genre TEXT, user_id INTEGER)');
     db.run('CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY, content TEXT, post_id INTEGER, user_id INTEGER)');
 });
 
-function registerUser(name, email, password) {
-    if (!name || !email || !password) {
+function registerUser(name, password) {
+    if (!name || !password) {
         return 'Please fill in all the fields';
     }
     const saltRounds = 10;
     const hash = bcrypt.hashSync(password, saltRounds);
-    db.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hash], (err) => {
+    db.run('INSERT INTO users (name, password) VALUES (?, ?)', [name, hash], (err) => {
         if (err) {
             console.error(err.message);
             return err.message;
@@ -31,6 +32,32 @@ function registerUser(name, email, password) {
     });
 }
 
+const dbGet = promisify(db.get.bind(db));
+
+async function loginUser(username, password) {
+    if (!username || !password) {
+        return 'Please fill in all the fields';
+    }
+    try {
+        const row = await dbGet('SELECT * FROM users WHERE username = ?', [username]);
+        if (row) {
+            const result = await comparePasswords(password, row.password);
+            if (result) {
+                console.log('User logged in successfully');
+                return 'User logged in successfully';
+            } else {
+                console.log('Invalid Credentials');
+                return 'Invalid Credentials';
+            }
+        } else {
+            return 'Invalid Credentials';
+        }
+    } catch (err) {
+        console.error(err.message);
+        return err.message;
+    }
+}
+
 function comparePasswords(password, hash) {
     return bcrypt.compare(password, hash);
 }
@@ -38,5 +65,6 @@ function comparePasswords(password, hash) {
 module.exports = {
     db,
     registerUser,
+    loginUser,
     comparePasswords
 };
--- index.js
+++ index.js
@@ -1,26 +1,58 @@
 const express = require('express');
 const path = require('path');
 var db = require('./db');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
-// Set the view engine to ejs
+const generalMiddleware = (req, res, next) => {
+    res.locals.message = req.query.msg;
+    next();
+};
+
+const loggedInMiddleware = (req, res, next) => {
+    if (req.cookies.user) {
+        jwt.verify(req.cookies.user, process.env.AUTH_SECRET, (err, decoded) => {
+            if (err) {
+                res.redirect('/auth/login');
+            } else {
+                next();
+            }
+        });
+        next();
+    } else {
+        res.redirect('/auth/login');
+    }
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-
-// Set the views directory
 app.set('views', path.join(__dirname, 'views'));
-
-// Set the public directory for static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(generalMiddleware);
 
 // Define a route
-app.get('/', (req, res) => {
+app.get('/', loggedInMiddleware, (req, res) => {
     res.render('index', { title: 'Home' });
 });
 
-app.post('/auth/login', (req, res) => {
-    
+app.get('/auth/login', (req, res) => {
+    res.render('login', { title: 'Login' });
+});
+
+app.post('/auth/login', async (req, res) => {
+    const { username, password } = req.body;
+    const result = await db.loginUser(username, password);
+    res.redirect("/?msg=" + await result);
 });
 
 // Start the server
