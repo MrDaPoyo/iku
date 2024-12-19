bfe65c58351bbd9eea453e74c6b3258f764ae4e8
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-12-19 09:11:52 +0000

    width

--- public/js/trackList.js
+++ public/js/trackList.js
@@ -5,38 +5,30 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         const trackListContainer = document.createElement('div');
         trackListContainer.className = 'track-list';
-        for (let i = 0; i < 10; i++) {
         songs.forEach(song => {
             const minutes = Math.floor(song.length / 60);
             const seconds = song.length % 60;
             const formattedLength = `${minutes}:${seconds.toString().padStart(2, '0')}`;
 
-            const card = `
-                <div class="track">
-                    <a href="/?track_id=${song.id}" class="track-link">
-                        <div class="track-image">
-                            <img src="https://placehold.co/600x400" alt="${song.title}">
-                        </div>
-                        <div>
-                            <h2 class="track-title">${song.title}</h2>
-                            <div class="track-artist"><h3>${song.artist}</h3> - ${song.album}</div>
-                        </div>
-                    </a>
-                    <div>
-                        <h4 class="track-date">${song.year}</h4>
-                        <h3 class="track-duration">${formattedLength}</h3>
+            const card = document.createElement('div');
+            card.className = 'track';
+            card.innerHTML = `
+                <a href="/?track_id=${song.id}" class="track-link">
+                    <div class="track-image">
+                        <img src="https://placehold.co/600x400" alt="${song.title}">
                     </div>
+                    <div>
+                        <h2 class="track-title">${song.title}</h2>
+                        <div class="track-artist"><h3>${song.artist}</h3> - ${song.album}</div>
+                    </div>
+                </a>
+                <div>
+                    <h4 class="track-date">${song.year}</h4>
+                    <h3 class="track-duration">${formattedLength}</h3>
                 </div>
             `;
-
-            const link = document.createElement('a');
-            link.id = `track-${song.id}`;
-            link.href = `/?track_id=${song.id}`;
-            link.innerHTML = card;
-
-            document.getElementById("trackContainer").appendChild(link);
+            document.getElementById("trackContainer").appendChild(card);
         });
-    }
     } catch (error) {
         console.error('Error fetching songs:', error);
     }
